f43443f58b5e84a115d7e52889c76f0197e249d7
chiaraintech <chiaraintech@gmail.com>
2021-04-30 13:24:43 +0100

    Pushing initial app.

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+      <div>
+          <Head>
+              <title>Hello World!</title>
+              <meta name="about-keywords" content="Introduction" />
+          </Head>
+        <h1>About</h1>
+      </div>
+    )
+  }
+  
+  export default about
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+
+      <Head>
+        <title>Webdev News</title>
+        <meta name="index-keywords" content="web development, programming"/>
+      </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
